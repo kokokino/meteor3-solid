e2fbb764a2706e77ba5b8980d5e1fd4eef97b082
Aaron Rosenzweig <info@kokokino.com>
2025-10-20 22:28:48 -0400

    3.4: Show Newest Tasks First

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -4,13 +4,26 @@ import { Tracker } from "meteor/tracker";
 import { TasksCollection } from "../api/TasksCollection";
 
 export const App = () => {
+  const [newTask, setNewTask] = createSignal('');
+
+  const addTask = async (event) => {
+    event.preventDefault();
+    if (newTask().trim()) {
+      await Meteor.callAsync("tasks.insert", {
+        text: newTask(),
+        createdAt: new Date(),
+      });
+      setNewTask('');
+    }
+  };
+
   const subscription = Meteor.subscribe("tasks");
   const [isReady, setIsReady] = createSignal(subscription.ready());
   const [tasks, setTasks] = createSignal([]);
 
   Tracker.autorun(async () => {
     setIsReady(subscription.ready());
-    setTasks(await TasksCollection.find().fetchAsync());
+    setTasks(await TasksCollection.find({}, { sort: { createdAt: -1 } }).fetchAsync());
   });
 
   return (
@@ -18,6 +31,16 @@ export const App = () => {
       <header>
         <h1>Todo List</h1>
       </header>
+
+      <form class="task-form" onSubmit={addTask}>
+        <input
+          type="text"
+          placeholder="Type to add new tasks"
+          value={newTask()}
+          onInput={(e) => setNewTask(e.currentTarget.value)}
+        />
+        <button type="submit">Add Task</button>
+      </form>
 
       <Show
         when={isReady()}
--- imports/ui/main.jsx
+++ imports/ui/main.jsx
@@ -3,6 +3,7 @@ import { render } from 'solid-js/web';
 import { App } from './App';
 import { Meteor } from "meteor/meteor";
 import './main.css';
+import "/imports/api/TasksMethods"; // this import allows for optimistic execution
 
 Meteor.startup(() => {
   render(() => <App/>, document.getElementById('root'));
